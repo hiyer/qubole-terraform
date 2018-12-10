e345737a1f455cc5aa92a54bff68237f7301fc30
Hariharan Iyer <hiyer@qubole.com>
2018-12-10 19:34:30 +0530

    Modularize public subnet configuration

---
+++ aws/modules/public_subnet/variables.tf
@@ -0,0 +1,24 @@
+variable "tags" {
+  type = "map"
+  description = "Map of tags to attach to security group and instance"
+}
+
+variable "whitelist_ip" {
+  type = "list"
+  description = "List of IPs to whitelist SSH from"
+}
+
+variable "prefix" {
+  type = "string"
+  description = "Prefix for 'name' tag"
+}
+
+variable "vpc_id" {
+  type = "string"
+  description = "VPC Id"
+}
+
+variable "subnet_cidr" {
+  type = "string"
+  description = "CIDR for the subnet"
+}

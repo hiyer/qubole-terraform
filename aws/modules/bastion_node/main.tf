df97d8c7250f8a4bdd43f7f92e1f016b23514b3c
Hariharan Iyer <hiyer@qubole.com>
2019-03-27 22:08:20 +0530

    Remove hard-coding of bastion-node instance type

--- aws/modules/bastion_node/main.tf
+++ aws/modules/bastion_node/main.tf
@@ -39,7 +39,7 @@ resource "aws_security_group" "bastion_node" {
         protocol = "tcp"
         cidr_blocks = ["${data.aws_vpc.default.cidr_block}"]
     }
-    
+
     # Allow all outgoing traffic to private subnet
     egress {
       from_port = 0
@@ -59,7 +59,7 @@ resource "aws_security_group" "bastion_node" {
 # Bastion Instance
 resource "aws_instance" "bastion_node" {
     ami = "${data.aws_ami.amzn_linux.id}"
-    instance_type = "t3.small"
+    instance_type = "${var.instance_type}"
     key_name = "${var.aws_key_name}"
     vpc_security_group_ids = ["${aws_security_group.bastion_node.id}"]
     subnet_id = "${var.public_subnet_id}"

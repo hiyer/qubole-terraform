e02eae42b1b606c2dde5423e389bd489e302b976
Hariharan Iyer <hiyer@qubole.com>
2019-07-09 09:55:04 +0530

    Revert "Upgrade network acl and private subnet to terraform 0.12"
    This reverts commit 877f5426587c2845967ef7df808c7a38b238fe8c.

--- aws/modules/network_acl/variables.tf
+++ aws/modules/network_acl/variables.tf
@@ -1,38 +1,37 @@
 variable "vpc_id" {
-  type        = string
+  type = "string"
   description = "Id of the VPC to create the network ACL in"
 }
 
 variable "ssh_whitelist_ip" {
-  type        = list(string)
+  type = "list"
   description = "IP(s) to whitelist SSH from, if any"
-  default     = []
+  default = []
 }
 
 variable "whitelist_outgoing" {
-  type        = string
+  type = "string"
   description = "CIDR block to whitelist outgoing traffic to"
 }
 
 variable "allow_outgoing_http" {
-  type        = string
+  type = "string"
   description = "Whether to allow outgoing HTTP requests"
-  default     = "0"
+  default = "0"
 }
 
 variable "prefix" {
-  type        = string
+  type = "string"
   description = "Prefix for 'Name' tag"
 }
 
 variable "subnet_ids" {
-  type        = list(string)
+  type = "list"
   description = "List of subnets to apply the ACL to"
 }
 
 variable "tags" {
-  type        = map(string)
+  type = "map"
   description = "Tags for the Network ACL resource"
-  default     = {}
+  default = {}
 }
-
---
+++ aws/modules/private_subnet/main.tf
@@ -0,0 +1,52 @@
+data "aws_vpc" "default" {
+  id = "${var.vpc_id}"
+}
+
+data "aws_availability_zones" "all" {
+
+}
+
+locals {
+  newbits = "${ceil(log(var.num_pvt_subnets + 1, 2))}"
+  sorted_azs = "${sort(data.aws_availability_zones.all.names)}"
+}
+
+module "network_acl" {
+  source = "../network_acl"
+
+  vpc_id = "${var.vpc_id}"
+  tags = "${var.tags}"
+  prefix = "${var.prefix}"
+  whitelist_outgoing = "${var.whitelist_outgoing}"
+  allow_outgoing_http = "1"
+  subnet_ids = ["${aws_subnet.private_subnet.*.id}"]
+}
+
+/*
+  Private Subnet
+*/
+resource "aws_subnet" "private_subnet" {
+  count = "${var.num_pvt_subnets}"
+  vpc_id = "${var.vpc_id}"
+  availability_zone = "${element(local.sorted_azs, count.index)}"
+  cidr_block = "${var.subnet_cidr != "" && var.num_pvt_subnets == 1 ? var.subnet_cidr : cidrsubnet(data.aws_vpc.default.cidr_block, local.newbits, count.index + 1)}"
+  tags = "${merge(
+          map("Name", "${var.prefix}-private-subnet-${element(local.sorted_azs, count.index)}"),
+          "${var.tags}"
+        )}"
+}
+
+resource "aws_route_table" "private_subnet" {
+  vpc_id = "${var.vpc_id}"
+
+  tags = "${merge(
+          map("Name", "${var.prefix}-private-subnet-rt"),
+          "${var.tags}"
+        )}"
+}
+
+resource "aws_route_table_association" "private_subnet" {
+    count = "${var.num_pvt_subnets}"
+    subnet_id = "${element(aws_subnet.private_subnet.*.id, count.index)}"
+    route_table_id = "${aws_route_table.private_subnet.id}"
+}
